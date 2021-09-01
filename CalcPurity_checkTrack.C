1ad25b7a35d36672e61d315e418d98cd3b909642
xilinliang <xlian046@ucr.edu>
2021-09-01 12:41:54 -0400

    Update on purity

--- CalcPurity_checkTrack.C
+++ CalcPurity_checkTrack.C
@@ -25,10 +25,10 @@ void CalcPurity_checkTrack(TString inputFile, const char* PIDSystem,  Int_t ID)
 	int Ebin=100;
 	int Zbin=20;
 	if (PIDSystem == "dualRICHag")
-	{int Ebin=50;double Emin=0.; double Emax=12.;}
+	{double Emin=0.; double Emax=50.;}
 
 	if (PIDSystem == "dualRICHcf")
-        {double Emin=12.; double Emax=50.;}	
+        {double Emin=0.; double Emax=50.;}	
 
 	if (PIDSystem == "barrelDIRCTrack")
 	{double Emin=0.; double Emax=50.;}
@@ -39,24 +39,29 @@ void CalcPurity_checkTrack(TString inputFile, const char* PIDSystem,  Int_t ID)
 	TH1D *h1Pi_resultZ= new TH1D("h1Pi_resultZ","Pion Purity by energy fraction z; z",Zbin,0.,1.);
         TH1D *h1K_resultZ= new TH1D("h1K_resultZ","Kaon Purity by energy fraction z; z",Zbin,0.,1.);
         TH1D *h1Pr_resultZ= new TH1D("h1Pr_resultZ","proton Purity by energy fraction z; z",Zbin,0.,1.);
+	TH1D *h1Pi_K_ratioE= new TH1D("h1Pi_K_ratioE","True Pion / True Kaon by track energy; E [GeV]",Ebin,Emin,Emax);
 
 
 	for (Int_t i=1; i<=Ebin ; ++i)
         {
 
-		Double_t ngood=0;
-		Double_t nbad=0;
-		ngood=Pi_goodE->GetBinContent(i);
-                nbad=Pi_badE->GetBinContent(i);
-		if ((ngood + nbad) == 0) {Double_t purity=0;} else {Double_t purity=ngood / (ngood+ nbad);}
+		Double_t ngoodPi=0;
+		Double_t nbadPi=0;
+		ngoodPi=Pi_goodE->GetBinContent(i);
+                nbadPi=Pi_badE->GetBinContent(i);
+		if ((ngoodPi + nbadPi) == 0) {Double_t purity=0;} else {Double_t purity=ngoodPi / (ngoodPi + nbadPi);}
                 h1Pi_resultE->SetBinContent(i,purity);
 
-		Double_t ngood=0;
-                Double_t nbad=0;
-                ngood=K_goodE->GetBinContent(i);
-                nbad=K_badE->GetBinContent(i);
-                if ((ngood + nbad) == 0) {Double_t purity=0;} else {Double_t purity=ngood / (ngood+ nbad);}
+		Double_t ngoodK=0;
+                Double_t nbadK=0;
+                ngoodK=K_goodE->GetBinContent(i);
+                nbadK=K_badE->GetBinContent(i);
+                if ((ngoodK + nbadK) == 0) {Double_t purity=0;} else {Double_t purity=ngoodK / (ngoodK+ nbadK);}
                 h1K_resultE->SetBinContent(i,purity);
+		
+		Double_t Pi_K_ratio=0.;
+		if (((ngoodPi + nbadPi) > 0)&&((ngoodK + nbadK) > 0)) {Pi_K_ratio= (ngoodPi + nbadPi) / (ngoodK + nbadK);}
+		h1Pi_K_ratioE->SetBinContent(i,Pi_K_ratio);
 
 		Double_t ngood=0;
                 Double_t nbad=0;
@@ -107,6 +112,30 @@ void CalcPurity_checkTrack(TString inputFile, const char* PIDSystem,  Int_t ID)
 	h1Pi_resultZ->Draw();
         h1K_resultZ->Draw();
         h1Pr_resultZ->Draw();
+
+	TCanvas* c1=new TCanvas("Pion_compare","Pion match and no match compare");
+	c1.cd();
+	Pi_goodE->SetLineColor(2);
+	Pi_goodE->Draw();
+	Pi_badE->SetLineColor(5);
+	Pi_badE->Draw("same");
+	c1->SaveAs("Pion_compare.pdf");
+	
+	TCanvas* c2=new TCanvas("Kaon_compare","Kaon match and no match compare");
+	c2.cd();
+	K_goodE->SetLineColor(2);
+	K_goodE->Draw();
+	K_badE->SetLineColor(5);
+	K_badE->Draw("same");
+	c2->SaveAs("Kaon_compare.pdf");
+
+	TCanvas* c3=new TCanvas("Proton_compare","Proton match and no match compare");
+	c3.cd();
+	Pr_goodE->SetLineColor(2);
+	Pr_goodE->Draw();
+	Pr_badE->SetLineColor(5);
+	Pr_badE->Draw("same");
+	c3->SaveAs("Proton_compare.pdf");
 
 	result_file->Write();
 	result_file->Close();
